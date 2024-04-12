d5d256b447f325fabe939b67ab5fb940f9df094a
Lucas Santana <santanafx@hotmail.com>
2024-04-12 20:43:27 -0300

    init: initial commit

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import "../src/global.css"
+import HomePage from './pages/HomePage'
+import Login from './pages/Login'
+import MatchMenu from './pages/MatchMenu'
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <HomePage />,
+  errorElement: <div>Error 404 not found</div>
+},
+{
+  path: "/login",
+  element: <Login />,
+  errorElement: <div>Error 404 not found</div>
+},
+{
+  path: "/matchMenu",
+  element: <MatchMenu />,
+  errorElement: <div>Error 404 not found</div>
+},
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
