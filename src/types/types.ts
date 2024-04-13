160de41f9cfa22ebf7fe65f5bbe61cf335f18ba2
Lucas Santana <santanafx@hotmail.com>
2024-04-12 22:14:06 -0300

    feat: added new type for components

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,9 @@ export interface RouteError {
   statusText?: string;
   message?: string;
 }
+export interface LoginContextType {
+  InputUserNameLogin: React.ComponentType;
+  InputUserPasswordLogin: React.ComponentType;
+  ButtonEnterLogin: React.ComponentType;
+  ButtonCancelLogin: React.ComponentType;
+}
